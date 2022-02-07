4120a54bb833cf0619ec9aaa758672a1727d6087
Tariq Horan <N00202437@iadt.ie>
2022-02-07 17:12:09 +0000

    color and sort function added

--- lesson3/p5Starter-main/sketch.js
+++ lesson3/p5Starter-main/sketch.js
@@ -1,19 +1,24 @@
 //Bar chart using loops and translations!
 
-let data = [345,30,120,390,200,100];
-let margin = 20;
+let data = [345,30,300];
+let sortedData;
+let colors;
+let margin = 30;
 let spacing = 15;
 let chartWidth = 400;
 let chartHeight = 400;
 //Bimdas 
-let remainingSpace = chartWidth - (margin*2) - (spacing*(data.length-1));
+let remainingSpace = chartWidth - (margin*2) - (spacing * (data.length-1));
 
 let barWidth  = remainingSpace/data.length;
+
+console.log(barWidth);
 
 function setup(){
     createCanvas(500,500);
     background(0);
-
+    colors = [color('blue'),color('orange'),color('cyan')]
+    sort(data)
 }
 
 function draw(){
@@ -21,10 +26,10 @@ function draw(){
     
     translate(50,450);
     //y Axis
-    strokeWeight(3);
+    strokeWeight(1);
     line(0,0,0,-400);
     //x Axis
-    strokeWeight(3);
+    strokeWeight(1);
     line(0,0,400,0);
 
     // rect(20,0,20,-100);
@@ -36,7 +41,7 @@ function draw(){
 
 function drawBars(){
     for (let i = 0; i < data.length; i++) {
-        fill(0,255,255);
+        fill(colors[i]);
         strokeWeight(0);
         rect(i*(barWidth+spacing),0,barWidth,-data[i]);        
     }
