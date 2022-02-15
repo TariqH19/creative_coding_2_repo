23a72aed5e69a0e411844064cbb8b8a68971a791
Tariq Horan <N00202437@iadt.ie>
2022-02-15 17:26:10 +0000

    Small changes to graph

--- lesson4/p5Starter-main/sketch.js
+++ lesson4/p5Starter-main/sketch.js
@@ -1,89 +1,115 @@
 //Bar chart using loops and translations!
 
-let data = [1200,8600,2200,3100,2200,2550,1000];
-let scalledData = [];
+let data = [1200, 8600, 2200, 3100, 2200, 2550, 1000];
+//To access an element in an array use the array name and the index number to access the properties of the object use dot notation
+
 let maxValue;
 
-let sortData = true;
-let sortedData;
+
+//let sortData = true;
+//let sortedData;
 let colors;
 let margin = 30;
 let spacing = 15;
 let chartWidth = 400;
 let chartHeight = 400;
 //Remember bimdas for expressions 
-let remainingSpace = chartWidth - (margin*2) - (spacing * (data.length-1));
-let barWidth  = remainingSpace/data.length;
+let remainingSpace = chartWidth - (margin * 2) - (spacing * (data.length - 1));
+let barWidth = remainingSpace / data.length;
 
 //Variables for ticks
 let numTicks = 10;
 let tickLength = 5;
-let tickSpace = chartHeight/numTicks;
-let tickIncrement = chartHeight/numTicks;
+let tickSpace = chartHeight / numTicks;
+let tickIncrement;
 
 
-function setup(){
-    createCanvas(500,500);
+function setup() {
+    createCanvas(500, 500);
     background(0);
-    colors = [color('blue'),color('orange'),color('cyan')];
-    if(sortData == true){
-        sort(data)
-    }
-    maxValue=max(data);
-    for (let i = 0; i < data.length; i++) {
-        let newValue = map(data[i], 0,maxValue,0,chartHeight);
-        scalledData.push(newValue);
-        
-    }
+    colors = [color('#F3646A'), color('#F68F6A'), color('#FAB666'), color('#FFE066')];
+    // if (sortData == true) {
+    //     sort(data)
+    // }
+
+    let listValues = data.map(function(x) { return x.value });
+    maxValue = max(listValues);
+
+    tickIncrement = int(maxValue / numTicks);
+
+
 }
 
-function draw(){
-    background(120);
+function draw() {
+    background(50);
 
     push();
-    translate(50,450);
-
-    //y Axis
-    strokeWeight(1);
-    line(0,0,0,-chartWidth);
-    //x Axis
-    strokeWeight(1);
-    line(0,0,chartWidth,0);
-
-    //Draw some ticks
-    // line(0,0,-tickLength,0);
-    // line(0,-10,-tickLength,-10);
-    // line(0,-20,-tickLength,-20);
-    // line(0,-30,-tickLength,-30);
+    translate(posX, posY);
     drawTicks();
-    translate(margin,0);
+    translate(margin, 0);
     drawBars();
+    translate(-margin, 0);
+    drawAxis();
     pop();
 }
 
-function drawBars(){
-    textSize(12);
-    textAlign(LEFT,BOTTOM);
-    for (let i = 0; i < scalledData.length; i++) {
-        fill(colors[i%colors.length]);
+function drawAxis() {
+    //y Axis
+    strokeWeight(1);
+    stroke(255);
+    line(0, 0, 0, -chartWidth);
+    //x Axis
+    strokeWeight(1);
+    stroke(255);
+    line(0, 0, chartWidth, 0);
+}
+
+function scaledData(_num) {
+    let newValue = map(_num, 0, maxValue, 0, chartHeight);
+    return newValue;
+}
+
+function drawBars() {
+
+
+    for (let i = 0; i < data.length; i++) {
+        fill(colors[i % colors.length]);
         strokeWeight(0);
-        rect(i*(barWidth+spacing),0,barWidth,-scalledData[i]);
-        noStroke();
-        fill(0);
-        text("Text",i*(barWidth+spacing),-scalledData[i]);        
+        rect(i * (barWidth + spacing), 0, barWidth, scaledData(-data[i].value));
+
+        if (showValues) {
+            noStroke();
+            fill(255);
+            textSize(12);
+            textAlign(CENTER, BOTTOM);
+            text(data[i].value, i * (barWidth + spacing) + barWidth / 2, scaledData(-data[i].value) - 3);
+        }
+
+        if (showLabels) {
+            noStroke();
+            fill(255);
+            textSize(12);
+            textAlign(CENTER, BOTTOM);
+            text(data[i].label, i * (barWidth + spacing) + barWidth / 2, 25);
+        }
+
     }
 }
 
-function drawTicks(){
+function drawTicks() {
     textSize(12);
-    textAlign(RIGHT,CENTER);
+    textAlign(RIGHT, CENTER);
     for (let i = 0; i <= numTicks; i++) {
-        stroke(0);
+        stroke(255, 100);
         strokeWeight(1);
-        line(0,i*-tickSpace,-tickLength,i*-tickSpace);
-        
+        line(0, i * -tickSpace, -tickLength, i * -tickSpace);
+
+        stroke(255, 40);
+        strokeWeight(1);
+        line(0, i * -tickSpace, chartWidth, i * -tickSpace);
+
         noStroke();
-        fill(0);
-        text(i*tickIncrement,-10,-i *tickSpace);     
+        fill(255, 100);
+        text(i * tickIncrement, -10, -i * tickSpace);
     }
 }
