841130d86b1d7267babb64550bb1041f176aca76
Tariq Horan <N00202437@iadt.ie>
2022-02-21 23:19:23 +0000

    classes added to both graphs small bug on horizontal graph

---
+++ lesson6/barChart.js
@@ -0,0 +1,121 @@
+class horizontalBarChart {
+    constructor(_data) {
+        this.data = _data;
+        this.posX = 50;
+        this.posY = 450;
+        this.tickWeight = 1;
+        this.numTicks = 10;
+        this.tickLength = 5;
+        this.margin = 30;
+        this.spacing = 15;
+        this.chartWidth = 400;
+        this.chartHeight = 400;
+        this.showLabels = true;
+        this.rotateLabels = true;
+        this.showValues = true;
+        this.remainingSpace;
+        this.barWidth;
+        this.tickSpace;
+        this.tickIncrement;
+        this.maxValue;
+        this.updateVals();
+    }
+    updateVals() {
+        this.remainingSpace = this.chartHeight - (this.margin * 2) - (this.spacing * (this.data.length - 1));
+        this.barWidth = this.remainingSpace / this.data.length;
+        this.tickSpace = this.chartWidth / this.numTicks;
+        let listValues = this.data.map(function(x) { return x.value });
+        this.maxValue = max(listValues);
+        this.tickIncrement = int(this.maxValue / this.numTicks);
+    }
+    render() {
+        push();
+        this.drawTicks();
+        pop();
+        push();
+        this.drawBars();
+        pop();
+        push();
+        this.drawAxis();
+        pop();
+
+    }
+
+    drawTicks() {
+        translate(this.posX, 450);
+        textSize(12);
+        textAlign(CENTER, CENTER);
+        for (let i = 0; i <= this.numTicks; i++) {
+            stroke(255, 100);
+            strokeWeight(1);
+            line(i * this.tickSpace, 0, i * this.tickSpace, -this.tickLength);
+
+            stroke(255, 40);
+            strokeWeight(1);
+            line(i * this.tickSpace, 0, i * this.tickSpace, -this.chartHeight);
+
+            noStroke();
+            fill(255, 100);
+
+            text(i * this.tickIncrement, i * this.tickSpace, 10);
+        }
+    }
+
+    drawAxis() {
+        //y Axis
+        translate(50, 450);
+        stroke(255);
+        strokeWeight(1);
+
+        line(0, 0, 0, -this.chartHeight);
+        //x Axis
+        stroke(255);
+        strokeWeight(1);
+
+        line(0, 0, this.chartWidth, 0);
+    }
+
+    scaledData(_num) {
+        let newValue = map(_num, 0, this.maxValue, 0, this.chartWidth);
+        return newValue;
+    }
+
+    drawBars() {
+        translate(50, this.posX + this.margin);
+        for (let i = 0; i < this.data.length; i++) {
+            fill(colors[i % colors.length]);
+            strokeWeight(0);
+            rect(0, i * (this.barWidth + this.spacing), -this.scaledData(-data[i].value), this.barWidth);
+
+            if (this.showValues) {
+                noStroke();
+                fill(255);
+                textSize(12);
+                textAlign(LEFT, TOP);
+                text(this.data[i].value, this.scaledData(this.data[i].value) + 5, i * (this.barWidth + this.spacing) + this.barWidth / 2);
+            }
+
+            if (this.showLabels) {
+                if (this.rotateLabels) {
+                    push();
+                    noStroke();
+                    fill(255);
+                    textSize(12);
+                    textAlign(CENTER, BOTTOM);
+                    translate(-20, ((this.barWidth + this.spacing) * i) + this.barWidth / 2);
+                    rotate(PI / 2);
+                    text(this.data[i].label, 0, 0);
+                    pop();
+                } else {
+
+                    noStroke();
+                    fill(255);
+                    textSize(12);
+                    textAlign(RIGHT, BOTTOM);
+                    text(this.data[i].label, 0, i * (this.barWidth + this.spacing) + this.barWidth / 2);
+                }
+            }
+
+        }
+    }
+}
--- lesson7/sketch.js
+++ lesson7/sketch.js
@@ -8,136 +8,29 @@ let data = [
     { value: 16, label: "Pears" }
 ];
 
-
-let maxValue;
-let posX = 50;
-let posY = 450;
-let showLabels = true;
-let rotateLabels = true;
-let showValues = true;
+let data2 = [
+    { value: 232, label: "Oranges" },
+    { value: 222, label: "Apples" },
+    { value: 432, label: "Bananas" },
+    { value: 162, label: "Pears" }
+];
 
 let colors;
-let margin = 30;
-let spacing = 15;
-let chartWidth = 400;
-let chartHeight = 400;
-//Remember bimdas for expressions 
-let remainingSpace = chartHeight - (margin * 2) - (spacing * (data.length - 1));
-let barWidth = remainingSpace / data.length;
-
-//Variables for ticks
-let tickWeight = 1;
-let numTicks = 10;
-let tickLength = 5;
-let tickSpace = chartHeight / numTicks;
-let tickIncrement;
-
+let barChart01;
+let barChart02;
 
 function setup() {
-    createCanvas(500, 500);
-    background(0);
+    createCanvas(1000, 950);
     colors = [color('#F3646A'), color('#F68F6A'), color('#FAB666'), color('#FFE066')];
-
-    let listValues = data.map(function(x) { return x.value });
-    maxValue = max(listValues);
-
-    tickIncrement = int(maxValue / numTicks);
-
-
+    barChart01 = new BarChart(data);
+    barChart02 = new BarChart(data2);
 }
 
 function draw() {
     background(50);
-    push();
-    translate(posX, 450);
-    drawTicks();
-    pop();
-
-    push();
-    translate(posX, posX + margin);
-    drawBars();
-    pop();
-
-    push();
-    translate(posX, posY);
-    translate(0, 0);
-    drawAxis();
-    pop();
-    fill(255);
-    textSize(32);
-    textAlign(CENTER, TOP);
-    text("Amount of fruit sold in 24hrs", width / 2, 10);
-
-}
-
-function drawAxis() {
-    //y Axis
-    strokeWeight(1);
-    stroke(255);
-    line(0, 0, 0, -chartHeight);
-    //x Axis
-    strokeWeight(1);
-    stroke(255);
-    line(0, 0, chartWidth, 0);
-}
-
-function scaledData(_num) {
-    let newValue = map(_num, 0, maxValue, 0, chartHeight);
-    return newValue;
-}
-
-function drawBars() {
-    for (let i = 0; i < data.length; i++) {
-        fill(colors[i % colors.length]);
-        strokeWeight(0);
-        rect(0, i * (barWidth + spacing), -scaledData(-data[i].value), barWidth);
-
-        if (showValues) {
-            noStroke();
-            fill(255);
-            textSize(12);
-            textAlign(LEFT, TOP);
-            text(data[i].value, scaledData(data[i].value) + 5, i * (barWidth + spacing) + barWidth / 2);
-        }
-
-        if (showLabels) {
-            if (rotateLabels) {
-                push();
-                noStroke();
-                fill(255);
-                textSize(12);
-                textAlign(CENTER, BOTTOM);
-                translate(-20, ((barWidth + spacing) * i) + barWidth / 2);
-                rotate(PI / 2);
-                text(data[i].label, 0, 0);
-                pop();
-            } else {
-
-                noStroke();
-                fill(255);
-                textSize(12);
-                textAlign(RIGHT, BOTTOM);
-                text(data[i].label, 0, i * (barWidth + spacing) + barWidth / 2);
-            }
-        }
-
-    }
-}
-
-function drawTicks() {
-    textSize(12);
-    textAlign(CENTER, CENTER);
-    for (let i = 0; i <= numTicks; i++) {
-        stroke(255, 100);
-        strokeWeight(tickWeight);
-        line(i * tickSpace, 0, i * tickSpace, -tickLength);
-
-        stroke(255, 40);
-        strokeWeight(tickWeight);
-        line(i * tickSpace, 0, i * tickSpace, -chartHeight);
-
-        noStroke();
-        fill(255, 100);
-        text(i * tickIncrement, i * tickSpace, 10);
-    }
+    barChart01.render();
+    barChart01.updateVals();
+    barChart02.render();
+    barChart02.updateVals();
+    barChart02.posX = 550;
 }
